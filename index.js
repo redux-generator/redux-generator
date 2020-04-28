e2dc5ae7657f41795c57b1d6d6e51b8853f919ca
Artem Stepanov <artem.s@twsgo.com>
2020-04-28 16:46:47 +0300

    master add eslint

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
+// const util = require('util');
+// const exec = util.promisify(require('child_process').exec);
 require('dotenv').config();
 const clear = require('clear');
-const path = require('path');
 
 const { INIT_ENTITY, INIT_REDUX } = require('./lib/constants');
 
@@ -12,7 +13,16 @@ const { initRedux } = require('./lib/controllers/init-redux');
 const { initReduxEntity } = require('./lib/controllers/init-redux-entity');
 
 const runScript = async (argv) => {
+  // const l = ['redux', 'react-redux', 'redux-saga'].map(async (el) => {
+  //   const { stdout, stderr } = await exec(`npm view ${el} version`);
+  //   return stdout;
+  // });
+  // // console.log('stdout:', stdout);
+  // const lol = await Promise.all(l);
+  // console.log('stderr:', lol);
+
   console.log('argv', argv);
+  console.log('isDev', process.env.NODE_ENV);
   const command = argv[0];
   switch (command) {
     case INIT_REDUX:
